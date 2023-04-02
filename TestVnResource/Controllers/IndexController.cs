2943f1af7eb316b6a4536de90227aa57d52e73a9
kngyen2507 <ntknguyen2507@gmail.com>
2023-04-03 02:17:49 +0700

    completed Test

--- TestVnResource/Controllers/IndexController.cs
+++ TestVnResource/Controllers/IndexController.cs
@@ -11,6 +11,8 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using TestVnResource.Models;
 using static TestVnResource.Models.VNR_InternShipEntities;
+using System.Collections;
+using Newtonsoft.Json;
 
 namespace TestVnResource.Controllers
 {
@@ -20,15 +22,40 @@ namespace TestVnResource.Controllers
         public ActionResult Index()
         {
             VNR_InternShipEntities db = new VNR_InternShipEntities();
+			dynamic op = new ExpandoObject();
 			// Lay ds cac Khoa Hoc
 			List<KhoaHoc> khoahocs = db.KhoaHocs.ToList();
 			// Lay ds cac Mon Hoc
 			List<MonHoc> monhocs = db.MonHocs.ToList();
 
-			ViewBag.paramMonHoc = monhocs;
-			ViewBag.paramKhoaHoc = khoahocs;
+			List<object> results = new List<object>();
 
-			return View();
+
+			op.MonHoc = monhocs;
+			op.KhoaHoc = khoahocs;
+
+			return View(op);
         }
+
+		[HttpPost]
+		public ActionResult ShowData(string id)
+		{
+			int input = Int32.Parse(id);
+
+			VNR_InternShipEntities db = new VNR_InternShipEntities();
+			List<MonHoc> monhocs = db.MonHocs.ToList();
+			List<string> results = new List<string>();
+
+			foreach (MonHoc monHoc in monhocs)
+			{
+				if (monHoc.KhoaHocID == input)
+				{
+					results.Add(monHoc.TenMonHoc);
+				}
+			}
+
+			var output = JsonConvert.SerializeObject(results);
+			return Json(output);
+		}
 	}
 }
